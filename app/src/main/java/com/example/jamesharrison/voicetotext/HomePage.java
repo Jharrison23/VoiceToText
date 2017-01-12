2bf300d872b92aea144d4059268ced0e16751a25
jharrison23 <jharrison23@knights.ucf.edu>
2017-01-12 14:45:20 -0500

    linked the list view in homepage as well as created arraylist and item Note for use once the database is created

--- app/src/main/java/com/example/jamesharrison/voicetotext/HomePage.java
+++ app/src/main/java/com/example/jamesharrison/voicetotext/HomePage.java
@@ -3,11 +3,23 @@ package com.example.jamesharrison.voicetotext;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class HomePage extends AppCompatActivity
 {
 
+    // Declared UI elements to be used on this view
     FloatingActionButton addNewNote;
+
+    ListView notesList;
+
+    // Create an arraylist which will store the notes in the database
+    ArrayList<Note> notesArrayList;
+
+    // Variable of type Note, to add to the notesArrayList
+    Note note;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -15,11 +27,16 @@ public class HomePage extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
+        // Call the method to initialize the UI elements
         initialize();
     }
 
     public void initialize()
     {
+        // Initialize the floating action button and link it to the addNewNote Button
         addNewNote = (FloatingActionButton) findViewById(R.id.addNewNote);
+
+        // Initialize the list view and link it to the notesList listview
+        notesList = (ListView) findViewById(R.id.notesList);
     }
 }
